bfc9876072bd07d26be605aabc1c6c722e478800
benzkji <bnzk@bnzk.ch>
2019-01-21 13:15:37 +0100

    refactor done :)

--- minimalist_cms/cms_toolbar/static/cms_toolbar/js/toolbar_edit_item.js
+++ minimalist_cms/cms_toolbar/static/cms_toolbar/js/toolbar_edit_item.js
@@ -9,7 +9,7 @@
         iframe_link: null,
 
         _create: function() {
-            this.my_iframe_link = this.element.attr('href');
+            this.iframe_link = this.element.attr('href');
             this.toolbar = $('.minimalist-cms-toolbar').data('minimalist_cms-cms_toolbar');
             this._on(this.element, {'click': this.on_edit_click});
         },
